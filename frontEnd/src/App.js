2599c5617121f4a594c87e0decb0fe7121892be2
Selmen Rayen <selmanerayene95@gmail.com>
2023-05-15 22:54:54 +0100

    add routes

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,29 +1,49 @@
-import Search from './components/Search'
-import Navbar from './components/Navbar';
-import './App.css';
-import {BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import About from './components/About';
-import ShowReport from './components/ShowReport'
+import Search from "./components/Search";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import About from "./components/About";
+import ShowReport from "./components/ShowReport";
 import Contact from "./components/Contact";
-import Footer from './components/Footer';
-import Login from './components/Login';
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Layout from "./components/Layout";
+import Pictures from "./components/Pictures";
+import Content from "./components/Content";
 function App() {
- 
   return (
     <>
-   <Router>
-      <Navbar />
-      <Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Content />} />
 
-          <Route path='/' element={<Search/>}/>
-          <Route path='/virustotal/:domain' element={<ShowReport/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/login' element={<Login/>}/>
-         
-      </Routes>
-      <Footer />
-    </Router> 
+            <Route path="virustotal">
+              <Route path=":domain" element={<ShowReport />} />
+            </Route>
+
+            <Route path="contact">
+              <Route index element={<Contact />} />
+            </Route>
+
+            <Route path="about">
+              <Route index element={<About />} />
+            </Route>
+
+            <Route path="login">
+              <Route index element={<Login />} />
+            </Route>
+            {/* CATCH ALL */}
+            <Route path="*" element={<Navigate to={"/"} replace />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
